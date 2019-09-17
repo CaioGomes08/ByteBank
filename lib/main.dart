23df30591e501a3402f2ae8dc9d60c8b2770fbb2
Caio Gomes <caiovini-2015@bol.com.br>
2019-09-17 20:12:38 -0300

    Formulário para cadastrar as transferencias

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,43 @@ class FormularioTransferencia extends StatelessWidget {
       appBar: AppBar(
         title: Text('Criando Transferência'),
       ),
-      body: Text('data'),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.account_circle),
+                labelText: 'Número da conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0.00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+         
+          RaisedButton(
+            onPressed: () {},
+            child: Text('Confirmar'),
+          )
+        ],
+      ),
     );
   }
 }
